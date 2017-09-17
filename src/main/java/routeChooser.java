ad9b2677b326f700926e9b11024e4a9afdab2ff2
sdcode <shdcn@sina.com>
2017-09-17 23:07:22 +0800

    基本最短路径GUI

---
+++ src/main/java/routeChooser.java
@@ -0,0 +1,11 @@
+import org.apache.batik.swing.JSVGCanvas;
+
+import javax.swing.*;
+
+public class routeChooser {
+    private JPanel choosePanel;
+    private JPanel showPanel;
+    private JPanel listPanel;
+    private JScrollPane listScroll;
+    private JSVGCanvas routePanel;
+}
